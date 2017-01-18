604d9cb13cc6b9ef5fa3d2f4d4355633ba16e96d
Lev Himmelfarb <lev@boylesoftware.com>
2017-01-18 17:22:13 -0500

    Initial common content.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# x2node-common
-Common utilities module for the X2 Framework for Node.js.
+# X2 Framework for Node.js | Common Utilities
+
+This is a common utilities module for the X2 Framework for Node.js. The module is included by other modules that comprise the X2 Farmework for Node.js and is not intended for direct custom application use.
---
+++ index.js
@@ -0,0 +1,53 @@
+/**
+ * Common utilities module.
+ *
+ * @module x2node-common
+ */
+'use strict';
+
+
+/////////////////////////////////////////////////////////////////////////////////
+// Externals.
+/////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Node.js <code>Error</code> object.
+ *
+ * @external Error
+ * @see {@link https://nodejs.org/dist/latest-v4.x/docs/api/errors.html#errors_class_error}
+ */
+
+/**
+ * ES6 <code>Promise</code> object.
+ *
+ * @external Promise
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise}
+ */
+
+
+/////////////////////////////////////////////////////////////////////////////////
+// Errors.
+/////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * General framework usage error, such as invalid arguments, inappropriate
+ * function call, etc.
+ *
+ * @extends external:Error
+ */
+exports.X2UsageError = class extends Error {
+
+	/**
+	 * Create new error to throw.
+	 *
+	 * @param {string} message The error description.
+	 */
+	constructor(message) {
+		super();
+
+		Error.captureStackTrace(this, this.constructor);
+
+		this.name = 'X2UsageError';
+		this.message = message;
+	}
+}
